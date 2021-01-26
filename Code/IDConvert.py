5f574b91ceb1572eef59ab844cd84978ef702988
MrRubberDucky99 <judahfuller@gmail.com>
2021-01-26 08:56:49 +0000

    Removing unwanted code files for 2.0 rewrite

--- Code/IDConvert.py
+++
@@ -1,6 +0,0 @@
-def IDConvert(AsciiID):
-    byte_array = str(AsciiID).encode()
-    binary_int = int.from_bytes(byte_array, "big")
-    return format(binary_int, '128b').replace(" ", "0")
-##print(IDConvert("test1234test1234"))
-print(IDConvert("HELPHELPHELPHELP"))
